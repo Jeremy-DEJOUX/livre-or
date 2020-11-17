99542d3a0eaa32561469bd1715f52501a07e6c27
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-17 12:46:44 +0100

    ajout du header et de l'image du chateau

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/index.css">
     <title>Acceuil</title>
 </head>
-<body>
+<body id="bou">
 <!-- ======================================================HEADER================================================ -->
-    <header>
+    <header id="index_header" class="flex align_center">
+        <ul id="ul_header_index" class="flex justify_around align_center">
+            <li><a href="index.php">Acceuil</a></li>
+            <li><a href="livre-or.php">Livre d'Or</a></li>
+            <li><a href="commentaire.php">Commentaire</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="profil.php">Profil</a></li>
+        </ul>
     </header>
 
 
@@ -16,7 +24,22 @@
 
 
 <!-- ========================================================MAIN=================================================== -->
-    <main>
+    <main class="flex align_center flex_column justify_around" id="main_index">
+        <section class="flex align_center justify_around" id="Index_image_chateau">
+        </section>
+
+
+        <section class="flex align_center justify_around" id="">
+            <article></article>
+
+            <article></article>
+        </section>
+
+
+        <section class="flex align_center justify_around" id=""></section>
+
+
+        <section class="flex align_center justify_around" id=""></section>
     </main>
 
 
@@ -26,7 +49,7 @@
 
 
 
-    
+
 <!-- =============================================================FOOTER========================================== -->
     <footer>
     </footer>
