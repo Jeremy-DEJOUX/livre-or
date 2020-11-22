77af602e10c37b55f4146291b3fed4918a81cad1
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-22 03:27:24 +0100

    mise en place du menu déroulant

--- pages/header.php
+++ pages/header.php
@@ -1,30 +1,30 @@
-<header class="flex justify_around align_center" id="header">
-  <nav id="nav" class="flex justify_around align_center">
-    <ul class="flex justify_around align_center" id="ul_header">
+<header class="flex align_center" id="header">
+  <nav class="flex align_center">
+    <ul class="flex align_center justify_around" id="Menu">
 
 <!-- ========================== IF WE ARE CONNECTED ============================= -->
 
       <?php if (isset($_SESSION['id'])) { ?>
 
-        <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
-        <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
-        <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
-        <li><i class='fas fa-user-alt'></i>
-          <ul>
-            <li><a class="" href="/Project Pool 2/livre-or/pages/profil.php"></a></li>
+        <li class="menu-html flex align_center"> <a href='/Project Pool 2/livre-or/index.php'>Acceuil</a> </li>
+        <li class="menu-html"> <a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a> </li>
+        <li class="menu-html"> <a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a> </li>
+        <li class="menu-html"> <i class='fas fa-user-alt'></i>
+          <ul id="submenu">
+            <li id="submenu_deconnexion"> <a class="" href="/Project Pool 2/livre-or/pages/profil.php"></a> </li>
           </ul>
         </li>
 
 <!--=========================== IF WE ARE DOESN'T CONNECTED =============================== -->
       <?php } else { ?>
 
-        <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
-        <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
-        <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
-        <li id="logo_profil"><i class='fas fa-user-alt'></i>
-          <ul id="submenu">
-            <li><a href='/Project Pool 2/livre-or/pages/connexion.php'>Connexion</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/inscription.php'>Inscription</a></li>
+        <li class="menu-html"> <a href='/Project Pool 2/livre-or/index.php'>Acceuil</a> </li>
+        <li class="menu-html"> <a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a> </li>
+        <li class="menu-html"> <a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a> </li>
+        <li class="menu-html"> Profil <i class='fas fa-user-alt'></i>
+          <ul class="submenu flex align_center">
+            <li id="submenu_connexion"> <a href='/Project Pool 2/livre-or/pages/connexion.php'>Connexion</a> </li>
+            <li id="submenu_inscription"> <a href='/Project Pool 2/livre-or/pages/inscription.php'>Inscription</a> </li>
           </ul>
         </li>
 
