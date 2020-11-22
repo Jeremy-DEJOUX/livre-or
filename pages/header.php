4e23c48ebdb351144df6f6d5c4644d0fb18c0dc7
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-22 02:03:09 +0100

    Ajustement Header

--- pages/header.php
+++ pages/header.php
@@ -1,26 +1,35 @@
-<header id="index_header" class="flex align_center">
-        <ul id="ul_header_index" class="flex justify_around align_center">
-          <?php if (isset($_SESSION['id'])) { ?>
-            <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
-            <li><i class='fas fa-user-alt'></i>
-              <ul>
-                <li><a class='' href='deconnexion.php'>Deconnexion</a></li>
-              </ul>
-            </li>
-          <?php
-        }
-        else {
-          ?>
-            <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/inscription.php'>Inscription</a></li>
+<header class="flex justify_around align_center" id="header">
+  <nav id="nav" class="flex justify_around align_center">
+    <ul class="flex justify_around align_center" id="ul_header">
+
+<!-- ========================== IF WE ARE CONNECTED ============================= -->
+
+      <?php if (isset($_SESSION['id'])) { ?>
+
+        <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
+        <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
+        <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
+        <li><i class='fas fa-user-alt'></i>
+          <ul>
+            <li><a class="" href="/Project Pool 2/livre-or/pages/profil.php"></a></li>
+          </ul>
+        </li>
+
+<!--=========================== IF WE ARE DOESN'T CONNECTED =============================== -->
+      <?php } else { ?>
+
+        <li><a href='/Project Pool 2/livre-or/index.php'>Acceuil</a></li>
+        <li><a href='/Project Pool 2/livre-or/pages/livre-or.php'>Livre d'Or</a></li>
+        <li><a href='/Project Pool 2/livre-or/pages/commentaire.php'>Commentaire</a></li>
+        <li id="logo_profil"><i class='fas fa-user-alt'></i>
+          <ul id="submenu">
             <li><a href='/Project Pool 2/livre-or/pages/connexion.php'>Connexion</a></li>
-            <li><a href='/Project Pool 2/livre-or/pages/profil.php'><i class='fas fa-user-alt'></i></a></li>;
-          <?php
-        }
-        ?>
-        </ul>
+            <li><a href='/Project Pool 2/livre-or/pages/inscription.php'>Inscription</a></li>
+          </ul>
+        </li>
+
+<!-- ==================================== END ==================================== -->
+      <?php } ?>
+    </ul>
+  </nav>
 </header>
