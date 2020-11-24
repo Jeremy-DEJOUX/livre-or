5a8835236ac997cf59f4689b46e972370474b137
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-24 22:26:02 +0100

    Fin du projet il reste juste le css si jamais

--- pages/livre-or.php
+++ pages/livre-or.php
@@ -1,8 +1,8 @@
 <?php require_once('../configs/config.php');
 
 $querry = mysqli_query($bdd, "SELECT id_utilisateur, commentaire FROM commentaires ORDER BY id DESC");
-
-
+$user = mysqli_query($bdd, 'SELECT commentaires.id_utilisateur, utilisateurs.login FROM commentaires INNER JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id');
+$id = mysqli_fetch_assoc($user);
  ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
@@ -18,17 +18,13 @@ $querry = mysqli_query($bdd, "SELECT id_utilisateur, commentaire FROM commentair
   <?php
 
   while ($a = mysqli_fetch_assoc($querry)) {
-    echo "L'utilisateur ".$a['id_utilisateur']." A l'aissé ce commentaire: <br />";
+    echo "L'utilisateur ".$id['login']." A l'aissé ce commentaire: <br />";
     echo $a['commentaire']."<br /> <br />";
+
   }
 
-
-
-
-
-
-
   ?>
+
 </main>
   </body>
 </html>
